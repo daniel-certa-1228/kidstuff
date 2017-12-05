32e08f56dec0a907e0b5821b31ded0cab9e59d6b
daniel-certa-1228 <mfdancerta@gmail.com>
2017-12-05 15:54:34 -0600

    basic login and logout functioning

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,13 @@ class UsersController < ApplicationController
     end
 
     def create
-
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to root_url, notice: 'Thank you for signing up!'
+        else
+            render :new
+        end
     end
 
     def edit
@@ -22,7 +28,7 @@ class UsersController < ApplicationController
 
     private
 
-        def customer_params
+        def user_params
             params.require(:user).permit(:email, :password, :password_confirmation, :user_name)
         end
 end
