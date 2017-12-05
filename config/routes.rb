d4e8ae8770426f9ba356ac1ee9b89e9c65348f06
daniel-certa-1228 <mfdancerta@gmail.com>
2017-12-05 12:03:55 -0600

    added bootstrap; basic file setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :activities
+  resources :assignments
+  resources :artworks
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  root 'home#index'
 end
