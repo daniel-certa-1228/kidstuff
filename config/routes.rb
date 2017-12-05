c77afb11d401cc9c04b2dca10e13d906d9b2cd43
daniel-certa-1228 <mfdancerta@gmail.com>
2017-12-05 15:02:33 -0600

    created child resource and schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :children
   resources :users
   resources :activities
   resources :assignments
--- db/migrate/20171205174141_create_users.rb
+++ db/migrate/20171205174141_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-
+      t.string :email, null:false
+      t.string :password_digest, null:false
+      t.string :user_name, null:false
       t.timestamps
     end
   end
