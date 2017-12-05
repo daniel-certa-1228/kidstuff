21eaefeb9dffb7376f993ca7452962c6dfbbb1cb
daniel-certa-1228 <mfdancerta@gmail.com>
2017-12-05 14:38:47 -0600

    set basic controller actions for user and sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+
+    end
+
+    def edit
+        
+    end
+
+    def show
+
+    end
+
+    def update
+
+    end
+
+    private
+
+        def customer_params
+            
+        end
 end
