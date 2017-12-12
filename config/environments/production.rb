b2f26e85f2f59cfe153d58c2730dfcaeb0c99cae
daniel-certa-1228 <mfdancerta@gmail.com>
2017-12-12 14:48:56 -0600

    fix env for main ( i think? )

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   port:                 587,
   domain:               'dancerta.tech',
   user_name:            'kidstuffapp@gmail.com',
-  password:             'kidstuff6969',
+  password:             ENV.fetch('KIDSTUFF_MAIL_PASSWORD'),
   authentication:       'plain',
   enable_starttls_auto: true  }
   # Code is not reloaded between requests.
