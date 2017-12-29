4be9815a59075489aff6e5d324d7d0715510b3aa
daniel-certa-1228 <mfdancerta@gmail.com>
2017-12-29 13:59:13 -0600

    model comments

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,11 +1,11 @@
 require "#{Rails.root}/lib/tasks/downcase.rb"
+# ^^ require for function to downcase all filenames
 class Activity < ApplicationRecord
     has_attached_file :avatar, filename_cleaner: ExtensionDowncase.new, styles: {  sm: "150X200", original: "900x1200" }
     validates_presence_of :avatar
     validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
     
     belongs_to :user
-    # belongs_to :child
 
     def self.fuzzy_content_search(search_string)
         search_string = "%" + search_string.downcase + "%"
